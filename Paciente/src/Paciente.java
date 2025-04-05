420cf5a2fedf8eb99f5eed874d5bd6fc77c350b4
SarahiOrta <sarahi.ortah@gmail.com>
2025-04-04 23:30:20 -0600

    Reto 1 Completado
    Reto 01: Registro básico de pacientes completado, Sarahí Orta Hinojos

---
+++ Paciente/src/Paciente.java
@@ -0,0 +1,16 @@
+//Reto 1: Sarahí Orta H.//
+public class Paciente {
+    //Características del paciente
+    String nombre;
+    int edad;
+    String Expediente;
+
+    //Método que muestra la información del paciente
+
+    public void mostrarinformación(){
+
+        System.out.println("paciente:" + nombre);
+    System.out.println("Edad:" + edad);
+    System.out.println("Expediente:" + Expediente);
+    }
+    }
---
+++ Paciente/src/Principal.java
@@ -0,0 +1,15 @@
+// Reto 1: Sarahí Orta H.
+public class Principal {
+    public static void main (String [] args){
+        // Crear un nuevo objeto de tipo paciente
+        Paciente paciente1 = new Paciente ();
+
+        // Asignar valores a los atributos
+        paciente1.nombre = "Lena Marie";
+        paciente1.edad = 25;
+        paciente1.Expediente = "EXP202409";
+
+                // Llamar al metodo mostrarinformación() del objeto
+paciente1.mostrarinformación();
+    }
+}
